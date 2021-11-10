c7f55b1a314e3963cae1003b9cfe70ca7990ef29
bxq2011hust <bxq2011hust@qq.com>
2021-11-10 16:46:33 +0800

    remove wrong licence

--- bcos-boostssl/network/ASIOInterface.h
+++ bcos-boostssl/network/ASIOInterface.h
@@ -1,19 +1,4 @@
 /**
- * @CopyRight:
- * bcos-boostssl is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * bcos-boostssl is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with bcos-boostssl.  If not, see <http://www.gnu.org/licenses/>
- * (c) 2021-2023 fisco-dev contributors.
- *
  * @brief: inteface for boost::asio(for unittest)
  *
  * @file AsioInterface.h
