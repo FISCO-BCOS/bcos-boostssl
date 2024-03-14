7c714fe42461e489ba16bd15d9c35757c2f48d29
Shareong <740310627@qq.com>
2024-03-14 14:16:52 +0800

    update bcos-utilities (#109)
    * update deps
    
    * unified boost version

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,22 +1,5 @@
 hunter_config(
-    Boost VERSION "1.76.0-local"
-    URL
-    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/FISCO-BCOS/deps/boost_1_76_0.tar.bz2
-    https://downloads.sourceforge.net/project/boost/boost/1.76.0/source/boost_1_76_0.tar.bz2
-    https://nchc.dl.sourceforge.net/project/boost/boost/1.76.0/boost_1_76_0.tar.bz2"
-    SHA1
-    8064156508312dde1d834fec3dca9b11006555b6
-    CMAKE_ARGS
-    CONFIG_MACRO=BOOST_UUID_RANDOM_PROVIDER_FORCE_POSIX
-)
-
-hunter_config(bcos-utilities VERSION 1.0.0-rc3-local
-    URL https://${URL_BASE}/FISCO-BCOS/bcos-utilities/archive/632cbeb0412d6ffa3c5217630fe6039f4092f3a4.tar.gz
-	SHA1 37b46cd3174ec1b66e28f19e6e8f31f209b61e4b
-    CMAKE_ARGS HUNTER_PACKAGE_LOG_BUILD=ON HUNTER_PACKAGE_LOG_INSTALL=ON
-)
-
-hunter_config(OpenSSL VERSION tassl_1.1.1b_v1.4-local
-    URL https://${URL_BASE}/FISCO-BCOS/TASSL-1.1.1b/archive/f9d60fa510e5fbe24413b4abdf1ea3a48f9ee6aa.tar.gz
-    SHA1 e56121278bf07587d58d154b4615f96575957d6f
+    OpenSSL VERSION tassl_1.1.1b
+    URL https://${URL_BASE}/FISCO-BCOS/TASSL-1.1.1b/archive/5144cc460da7b7de36e27780aba0fd928da222ed.tar.gz
+    SHA1 e0d6de3a5abb2ea09cdf3fd22e50bd8b866125d6
 )
